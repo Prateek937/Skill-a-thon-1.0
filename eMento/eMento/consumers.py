e79ec0d084e4b005707a7dfdcf6c75832f566cf2
Prateek <prateeksanodiya937@gmail.com>
2023-02-23 16:33:04 +0530

    new version

---
+++ eMento/eMento/consumers.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+def ConsumerRegister():
+    #code
+    return render('')
+
+def ConsumerLogin():
+    #code
+    return render("")
+
+def EnrolledCourses():
+    #code
+    return render("")
+
+def Mentor():
+    #code
+    return render("")
+
+def Feedback():
+    #code
+    return render("")
+
+def Message():
+    #code
+    return render("")
+def Assesment():
+    #code
+    return render('')
+    
