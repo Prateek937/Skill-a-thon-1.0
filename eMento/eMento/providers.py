a9777bc0bea8752fc434e7de8d1515c8ab06991d
Prateek <prateeksanodiya937@gmail.com>
2023-02-23 17:44:13 +0530

    second update

---
+++ eMento/eMento/providers.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def ProviderLogin():
+    #code
+    return render("")
+
+def Account():
+    #code
+    return render("")
+
+def Feedback():
+    #code
+    return render("")
+
+def Application():
+    #code
+    return render("")
+
+def JobApplication():
+    #code
+    return render("")
+    
---
+++ eMento/eMento/staffView.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+def StaffLogin(request):
+    #code
+    return render("#")
+
+def ViewStaffDetails(request):
+    #code
+    return render("#")
+
+def AddConsumers(request):
+    #code
+    return render("#")
+
+def displayConsumers(request):
+    #code
+    return render("#")
+
+def EditConsumerDetails(request):
+    #code
+    return render("#")
+
+def UploadResources(request):
+    #code
+    return render("#")
+
+def ViewReport(request):
+    #code
+    return render("#")
+
+def feedback(request):
+    # code
+    return render("#")
+
+def Message(request):
+    #code
+    return render("#")
+
+def AssesmentRecord(request):
+    #code
+    return render("#")
